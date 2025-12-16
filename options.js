bd516d7000becc3946461b7a8ee2bad4f61c81f9
Jim Hollenbach <jim@buildbuddy.io>
2025-12-15 16:42:22 -0800

    its 2025 and extensions are different now

--- options.js
+++ options.js
@@ -26,6 +26,24 @@ function saveOptions() {
       // Update status to let user know options were saved.
       const status = document.getElementById('status');
       status.textContent = 'saved!';
+      chrome.declarativeNetRequest.getDynamicRules().then((r) => {
+        newRules = always.split('\n').map(value => value.trim()).filter((v) => v != "").map((t, i) => {return {
+          action: {
+            type: "redirect",
+            redirect: {
+              url: 'data:text/html;base64,ICA8IURPQ1RZUEUgaHRtbD4NCiAgPGh0bWw+PGhlYWQ+PHRpdGxlPk5JQ0UgVFJZPC90aXRsZT48L2hlYWQ+PGJvZHk+PHNwYW4gc3R5bGU9ImZvbnQtZmFjZTogYm9sZDsgZm9udC1zaXplOiAzMGVtOyI+Tk9QRTwvc3Bhbj48L2JvZHk+PC9odG1sPg=='
+            }
+          },
+          condition: {
+            urlFilter: "*://*" + t + "*/*",
+            resourceTypes: ["main_frame"]
+          },
+          id: i+1
+        }});
+        const oldIds = r.map((v) => v.id)
+        chrome.declarativeNetRequest.updateDynamicRules({addRules: newRules, removeRuleIds: oldIds})
+      });
+
       setTimeout(() => { status.textContent = ''; }, 750);
     },
   );
